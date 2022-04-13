cf67750e276a15ffc133d8ca3bb7c51d3117cc7c
Stevennnncz <steven070295@gmail.com>
2022-04-12 23:09:59 -0600

    diccionarios

--- base_de_datos.py
+++ base_de_datos.py
@@ -1,10 +1,30 @@
 import hashlib
 
+
+carreras = [
+    {
+        'carrera' : 'Ingenieraia en computacion',
+    },
+    {
+        'carrera' : 'Administracion de empresas'
+    },
+    {
+        'carrera' : 'Administracion electronica'
+    },
+    {
+        'carrera' : 'Ingenieria en agronomia'
+    },
+    {
+        'carrera' : 'Ingenieria en procuccion industrial'
+    }
+        ]
+
 cursos = [
     {
         'curso' : "Fundamentos de organización de computadores",
         'creditos' : (2),
-        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo"
+        'código',
     }
 ]
 
