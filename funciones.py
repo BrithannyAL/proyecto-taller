7ccd0993c14bb5f6dff9c9583a5f3118cb4f4610
Stevennnncz <steven070295@gmail.com>
2022-04-12 22:22:43 -0600

    insercion de funciones

--- funciones.py
+++ funciones.py
@@ -1,9 +1,14 @@
-def funcion(n):
-    for x in range(0, n):
-        n = n + 1
-    return n
+import hashlib
+import getpass
+import time
 
-print(funcion(5))
+def cifrar (entrada):
+    entrada_c=entrada.encode('ascii')
+    resultado = hashlib.md5(entrada_c)
+    return (resultado.hexdigest())
 
-print ("hola")
-print("hola también")
+
+def obtener_calve(mensaje):
+    pswd = getpass.getpass(mensaje+": ")
+    return (pswd)
+
