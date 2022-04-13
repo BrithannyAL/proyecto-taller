db1455370c6d2454d4bc94aefad969c3a1af0dac
Stevennnncz <steven070295@gmail.com>
2022-04-12 23:10:30 -0600

    diccionarios

--- base_de_datos.py
+++ base_de_datos.py
@@ -4,27 +4,147 @@ import hashlib
 carreras = [
     {
         'carrera' : 'Ingenieraia en computacion',
+        'semestres' : (8),
+        'cursos' : {}
     },
     {
-        'carrera' : 'Administracion de empresas'
+        'carrera' : 'Administracion de empresas',
+        'semestres' : (10),
     },
     {
-        'carrera' : 'Administracion electronica'
+        'carrera' : 'Administracion electronica',
+        'semestres' : (10),
     },
     {
-        'carrera' : 'Ingenieria en agronomia'
+        'carrera' : 'Ingenieria en agronomia',
+        'semestres' : (8),
     },
     {
-        'carrera' : 'Ingenieria en procuccion industrial'
+        'carrera' : 'Ingenieria en procuccion industrial',
+        'semestres' : (12),
     }
         ]
 
 cursos = [
     {
-        'curso' : "Fundamentos de organización de computadores",
+        'curso' : "Matematica general",
         'creditos' : (2),
-        'Profesor' : "Rocio De Los Angeles Quiros Oviedo"
-        'código',
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (1)
+    },
+    {
+        'curso' : "Comunicacion escrita",
+        'creditos' : (4),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (2)
+    }, 
+    {
+        'curso' : "Ingles 1",
+        'creditos' : (6),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (3)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (4)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (5)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (6)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (7)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (8)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (9)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (10)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (11)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (12)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (13)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (14)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (15)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (16)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (17)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (18)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (19)
+    },
+    {
+        'curso' : "Matematica general",
+        'creditos' : (2),
+        'Profesor' : "Rocio De Los Angeles Quiros Oviedo",
+        'código' : (20)
     }
 ]
 
