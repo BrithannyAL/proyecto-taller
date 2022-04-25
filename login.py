9257cfd99ddf984b6e61a92931fe16fd01ea8204
BrithannyAL <49217000+BrithannyAL@users.noreply.github.com>
2022-04-25 09:48:40 -0600

    formato a registro de estudiante

--- login.py
+++ login.py
@@ -1,9 +1,10 @@
-#Se importan los dstos de los usuarios y las funciones para sus respectivas interfaces
+# Se importan los dstos de los usuarios y las funciones para sus respectivas interfaces
 from tkinter import E
 from base_de_datos import usuarios, estudiantes, carreras, cursos
 from funciones_admin import agregar_curso, modificar_curso, agregar_carrera, modificar_carrera
 from funciones_estudiante import generar_reporte, matricular_carrera, matricular_curso, generar_reporte, registro_actividades, aprobado_noAprobado, ver_horario
 import hashlib
+
 
 def inicio():
     """
@@ -19,10 +20,11 @@ Digite x si desea cerrar el software
         registrar()
     elif opcion == 'x':
         quit()
-    else: 
+    else:
         print("")
         print("Por favor ingrese una opcion valida")
         inicio()
+
 
 def login():
     """
@@ -51,6 +53,7 @@ def login():
         if correcto == False:
             print("Vuelva a intentar")
 
+
 def registrar():
     """
         Esta es la segunda opción del menú principal. La función permite registrar un nuevo usuario de tipo estudiante o admin. Primero, el sistema le pide los datos necesarios al usuario que está creando la cuenta, este mismo verifica si el usuario ingresado ya está regstrado en el sistema. Una vez que el sisteema haya recolectado los datos para la creación de la cuenta, verifica el tipo indicado por el usuario para darle la forma adecuada dentro de la base de datos.
@@ -69,14 +72,14 @@ def registrar():
                 print('El nombre de usuario no se encuentra disponible')
         else:
             usuarios.append({
-            'nombre' : nombre,
-            'tipo' : tipo,
-            'telefono' : telefono,
-            'autenticacion' :{
-                'usuario' : usuario,
-                'contraseña' : hashlib.md5(contraseña.encode('ascii')).hexdigest()
-                                } 
-                                })
+                'nombre': nombre,
+                'tipo': tipo,
+                'telefono': telefono,
+                'autenticacion': {
+                    'usuario': usuario,
+                    'contraseña': hashlib.md5(contraseña.encode('ascii')).hexdigest()
+                }
+            })
         print("""El administrador se ha ingresado correctamente
         """)
         inicio()
@@ -86,52 +89,54 @@ def registrar():
                 print('El nombre de usuario no se encuentra disponible')
         else:
             estudiantes.append({
-            'nombre' : nombre,
-            'tipo' : tipo,
-            'tiempo' : int,
-            'estudios':
+                'nombre': nombre,
+                'tipo': tipo,
+                'tiempo': int,
+                'estudios':
                 {
-                'carreras': [],
-                'cursos': [],
+                    'carreras': [],
+                    'cursos': [],
                 },
-            'autenticacion' :{
-                'usuario' : usuario,
-                'contraseña' : hashlib.md5(contraseña.encode('ascii')).hexdigest()
-                             },
+                'autenticacion': {
+                    'usuario': usuario,
+                    'contraseña': hashlib.md5(contraseña.encode('ascii')).hexdigest()
+                },
                 'horario':
-                            {
-            'lunes':      {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-            'martes':     {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-            'miercoles':  {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-            'jueves':     {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-            'viernes':    {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-            'sabado':     {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-            'domingo':    {7: [], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[], 14:[], 15:[], 16:[], 17:[], 18:[], 19:[], },
-
-                            },
-                'reporte' : 
                 {
-                    'lunes':      {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{} },
-                    'martes':     {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{}},
-                    'miercoles':  {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{} },
-                    'jueves':     {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{}},
-                    'viernes':    {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{}},
-                    'sabado':     {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{}},
-                    'domingo':    {7: {}, 8: {}, 9: {}, 10:{}, 11:{}, 12:{}, 13:{}, 14:{}, 15:{}, 16:{}, 17:{}, 18:{}, 19:{}, 20:{}, 21:{}, 22:{}, 23:{}, 24:{}},
+                    'lunes':      {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+                    'martes':     {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+                    'miercoles':  {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+                    'jueves':     {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+                    'viernes':    {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+                    'sabado':     {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+                    'domingo':    {7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], },
+
+                },
+                'reporte':
+                {
+                    'lunes':      {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
+                    'martes':     {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
+                    'miercoles':  {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
+                    'jueves':     {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
+                    'viernes':    {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
+                    'sabado':     {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
+                    'domingo':    {7: {}, 8: {}, 9: {}, 10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {}, 19: {}, 20: {}, 21: {}, 22: {}, 23: {}, 24: {}},
                 }
-                                })
+            })
         print("""El estudiante se ha ingresado correctamente
         """)
         inicio()
     else:
         print("Este tipo de usuario no es válido")
-        inicio() 
-            
-#Según el tipo de usuario se mostrará una interfaz diferente definida por su tipo de usuario
+        inicio()
+
+# Según el tipo de usuario se mostrará una interfaz diferente definida por su tipo de usuario
+
+
 def menu(tipo, nombre):
     """
         Esta función imprime el menú de opciones para cada tipo de usuarios (admin/estudiante). La función detecta el usuario usuario que le llega y apartir de este geberal el menú de opciones, las cuales son diferentes entre los tipos de usuarios. Para esto se utiliza un condicional. Después de haber impreso el menú de opciones, se le pedirá al usuario que seleccione una, y esta se enviará como parámetro a otra función.
-        
+
         Los parametros de esta función son:
         tipo: str = este es el tipo de cuenta que ha iniciado sesión, lo necesitamos para que el sistema sepa cuál es el menú que debe imprimir.
         nombre: str = el nombre del usuario de la cuenta, este se usa para imprimir el saludo de bienvenida."""
@@ -161,11 +166,12 @@ def menu(tipo, nombre):
         opcion = int(input("¿Qué acción desea realizar? "))
         funciones_estudiante(opcion)
 
+
 def funciones_admin(opcion):
     global carreras
     global cursos
     if opcion == 1:
-       cursos = agregar_curso(list(cursos))
+        cursos = agregar_curso(list(cursos))
     elif opcion == 2:
         cursos = modificar_curso(list(cursos))
     elif opcion == 3:
@@ -178,21 +184,24 @@ def funciones_admin(opcion):
             quit()
         login()
 
+
 def funciones_estudiante(opcion):
     global carreras
     global cursos
     global estudiantes
     global usuario
     if opcion == 1:
-         estudiantes = matricular_carrera(usuario, carreras, cursos, estudiantes)
+        estudiantes = matricular_carrera(
+            usuario, carreras, cursos, estudiantes)
     elif opcion == 2:
         estudiantes = matricular_curso(usuario, carreras, cursos, estudiantes)
     elif opcion == 3:
         estudiantes = generar_reporte(usuario, carreras, cursos, estudiantes)
     elif opcion == 4:
-        estudiantes = registro_actividades(usuario, carreras, cursos, estudiantes)
+        estudiantes = registro_actividades(
+            usuario, carreras, cursos, estudiantes)
     elif opcion == 5:
-       aprobado_noAprobado(usuario, estudiantes, cursos)
+        aprobado_noAprobado(usuario, estudiantes, cursos)
     elif opcion == 6:
         ver_horario(usuario, estudiantes)
     elif opcion == 7:
