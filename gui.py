4fad6121e6efe04b1d7704d53bddfbe621a82122
BrithannyAL <49217000+BrithannyAL@users.noreply.github.com>
2022-05-15 21:45:34 -0600

    guardar

--- gui.py
+++ gui.py
@@ -1,47 +1,70 @@
+import msvcrt
+import shutil
 from cgitb import text
 import tkinter as tk
+from tkinter import ttk
 from tkinter import *
-from turtle import width
-from typing import Text
-from email.mime import base
+import login
 
+#shutil.copy('txt.txt', 'base_de_datos.py')
+#msvcrt.getch()
 
+def imprimir():
+    print("hola mundo")
 
+def ventana_login():
+    ventana_login = tk.Tk()
 
-ventana = tk.Tk()
-ventana.title('Proyecto Taller')
-ventana.minsize(800,600)
+    ventana_login.title('Proyecto Taller')
+    ventana_login.minsize(800,600)
 
-def cerrar_ventana():
-    ventana.destroy()
+    def cerrar_ventana_login():
+        ventana_login.destroy()
 
+    def hide(x):
+        for i in x:
+            i.pack_forget()
 
-btn_login = tk.Button(ventana,text = 'Iniciar sesion', command=login.login())
-btn_login.place(x=10,y=10)
+    lb_usuario = tk.Label(ventana_login,text='Ingrese su nombre de usuario: ')
+    lb_usuario.place(x=10, y=10)
 
-btn_login.pack
+    sv_usuario = tk.StringVar()
+    e_usuario = ttk.Entry(ventana_login,textvariable = sv_usuario, width = 40)
+    e_usuario.place(x=180,y=10) 
 
-#btn_reg = tk.Button(ventana,text = 'Registrarse', command=login.registrar())
-#btn_reg.place(x=10,y=40)
+    lb_usuario.pack()
+    e_usuario.pack()
+    lb_usuario.pack_forget()
+    e_usuario.pack_forget()
 
-btn_salir = tk.Button(ventana,text = 'Salir', command=cerrar_ventana)
-btn_salir.place(x=10,y=70)
+    lb_contra = tk.Label(ventana_login,text='Ingrese su contrasena: ')
+    lb_contra.place(x=10, y=10)
 
+    sv_contrasena = tk.StringVar()
+    e_contra = ttk.Entry(ventana_login,textvariable = sv_contrasena, width = 40)
+    e_contra.place(x=180,y=10) 
 
-''' lb_usuario = tk.Label(ventana,text='Ingrese su nombre de usuario: ')
-lb_usuario.place(x=10, y=10)
+    lb_contra.pack()
+    e_contra.pack()
+    lb_contra.pack_forget()
+    e_contra.pack_forget()
 
-sv_usuario = tk.StringVar()
-e_usuario = ttk.Entry(ventana,textvariable = sv_usuario, width = 40)
-e_usuario.place(x=180,y=10) '''
+    btn_login = tk.Button(ventana_login,text = 'Iniciar sesion')
+    btn_login.place(x=60,y=100)
 
+    btn_reg = tk.Button(ventana_login,text = 'Registrarse')
+    btn_reg.place(x=10,y=40)
 
+    btn_salir = tk.Button(ventana_login,text = 'Salir')
+    btn_salir.place(x=80,y=100)
 
+    btn_login.configure(command=lambda: [hide([btn_salir,btn_login,btn_reg]),lb_usuario.pack(), e_usuario.pack(),lb_contra.pack(),e_contra.pack()])
+    btn_salir.configure(command= cerrar_ventana_login)
 
+    btn_login.pack()
+    btn_salir.pack()
+    btn_reg.pack()
 
-
-
-
-
-
-tk.mainloop()
+    tk.mainloop()
+    
+ventana_login()
--- index.py
+++ index.py
@@ -1,13 +1,3 @@
-#Desde aquí se llama a la función que realiza el login y al mismo timepo une todas los archivos
-import msvcrt
-from login import inicio
-import shutil
+from gui import ventana_login
 
-
-shutil.copy('txt.txt', 'base_de_datos.py')
-
-inicio()
-
-msvcrt.getch()
-
-
+ventana_login()
